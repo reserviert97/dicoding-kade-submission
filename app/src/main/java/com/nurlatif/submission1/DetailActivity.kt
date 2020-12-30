a59471c0924f5f78a9ca21a5f038c16df94a6318
ardhi <nurlatif97@outlook.com>
2020-12-31 02:58:52 +0700

    add anko for loging, toast, and start new activity

--- app/src/main/java/com/nurlatif/submission1/DetailActivity.kt
+++ app/src/main/java/com/nurlatif/submission1/DetailActivity.kt
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_detail.*
+import com.nurlatif.submission1.R.layout.activity_detail
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(), AnkoLogger {
 
     companion object {
         val ITEM_KEY = "league_data"
@@ -13,9 +16,11 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        setContentView(activity_detail)
 
         val league = intent.extras?.getParcelable<Item>(ITEM_KEY)
+
+        debug("[DetailActivity] Successfully pass data : ${league?.name}")
 
         detail_title.text = league?.name
         detail_id.text = league?.id.toString()
--- app/src/main/java/com/nurlatif/submission1/MainActivity.kt
+++ app/src/main/java/com/nurlatif/submission1/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.nurlatif.submission1
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.widget.Toast
 import com.nurlatif.submission1.R.array.*
 import com.nurlatif.submission1.R.layout.activity_main
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
 
     private var items: MutableList<Item> = mutableListOf()
 
@@ -23,12 +24,10 @@ class MainActivity : AppCompatActivity() {
 
         league_recyclerview.layoutManager = GridLayoutManager(this, 2, RecyclerView.VERTICAL, false)
         league_recyclerview.adapter = RecyclerViewAdapter(this, items){
-            val toast = Toast.makeText(applicationContext, it.name, Toast.LENGTH_SHORT)
-            toast.show()
-            val intent = Intent(this, DetailActivity::class.java)
-            intent.putExtra(DetailActivity.ITEM_KEY, it)
-            startActivity(intent)
+            toast(it.name)
+            startActivity<DetailActivity>(DetailActivity.ITEM_KEY to it)
         }
+
 
     }
 
@@ -43,7 +42,7 @@ class MainActivity : AppCompatActivity() {
             items.add(Item(id[i], name[i], desc[i], image.getResourceId(i, 0)))
         }
 
-        //Recycle the typed array
+        debug("[MainActivity] total leagues data : ${items.size}")
         image.recycle()
     }
 }
